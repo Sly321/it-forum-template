0f23a4fc2f9aad3ea87a50c2c497277e48fc69fa
Sven Liebig <liebigsv@gmail.com>
2017-03-05 17:56:36 +0100

    removing the navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 /** Angular Modules */
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
 
 /** Components */
 import { Wrapper } from './components/wrapper/wrapper.component';
@@ -9,17 +11,19 @@ import { Landing } from './components/landing/landing.component';
 import { Header } from './components/header/header.component';
 
 /** Third Party Angular */
+import { AUTH_PROVIDERS } from 'angular2-jwt';
 import '../node_modules/rxjs/add/operator/toPromise';
 
 /** Services */
-// TODO
+import { Authentification } from './services/authentification/authentification.service';
 
 /** Routes */
 import { routes } from './app.routes';
 
 @NgModule({
-	imports: [BrowserModule, RouterModule.forRoot(routes)],
+	imports: [FormsModule, HttpModule, BrowserModule, RouterModule.forRoot(routes)],
 	declarations: [Wrapper, Landing, Header],
-	bootstrap: [Wrapper]
+	bootstrap: [Wrapper],
+	providers: [Authentification, ...AUTH_PROVIDERS]
 })
 export class AppModule { }
