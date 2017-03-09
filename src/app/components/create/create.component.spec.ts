5ccf9e95b9a09f18d9004f1f8625c379be755a31
Sven Liebig <liebigsv@gmail.com>
2017-03-09 17:13:34 +0100

    test imports

--- src/app/components/create/create.component.spec.ts
+++ src/app/components/create/create.component.spec.ts
@@ -4,6 +4,8 @@
  * ...				9.3.2017				Created
  */
 
+import { FormsModule } from '@angular/forms';
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
@@ -36,6 +38,7 @@ describe('Create', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
+			imports: [FormsModule],
 			declarations: [Create, Post, Header],
 			providers: [Firebase]
 		}).overrideComponent(Create, {
