358a24a8101ffb75fddda9de8429c35c9fdd6d2a
Sven Liebig <liebigsv@gmail.com>
2017-03-14 12:51:43 +0100

    test coverage

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,8 @@ var gulp = require('gulp');
 var util = require('gulp-util');
 var tsc = require('gulp-typescript');
 var sourcemaps = require('gulp-sourcemaps');
-//var mocha = require('gulp-mocha');
-//var minify = require('gulp-minify');
 var runSequence = require('run-sequence');
+
 
 function handleError(err) {
     console.log(err.toString());
@@ -14,7 +13,7 @@ function handleError(err) {
 gulp.task('compile-ts', ['compile-src', 'compile-test']);
 
 gulp.task('compile-test', function () {
-    var tsProject = tsc.createProject('./src/tsconfig.json');
+	var tsProject = tsc.createProject('./src/tsconfig.json');
     return gulp.src(['./e2e/*.ts'])
         .pipe(tsProject())
         .on('error', util.log)
@@ -22,7 +21,7 @@ gulp.task('compile-test', function () {
 });
 
 gulp.task('compile-src', function () {
-    var tsProject = tsc.createProject('./src/tsconfig.json');
+	var tsProject = tsc.createProject('./src/tsconfig.json');
     return gulp.src(['./src/app/**/*.ts'])
 		.pipe(sourcemaps.init())
         .pipe(tsProject())
@@ -30,20 +29,6 @@ gulp.task('compile-src', function () {
 		.pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./src/app/'));
 });
-
-/*
-gulp.task('test', function () {
-    return gulp.src('./test/*.js', { read: false })
-    // gulp-mocha needs filepaths so you can't have any plugins before it
-        .pipe(mocha({ reporter: 'spec' }))
-        .on('error', handleError);
-});
-
-gulp.task('watch', function () {
-    gulp.watch(['./test/*.ts', './src/app/*.ts'], function () {
-        runSequence('compile-ts', 'test');
-    });
-});*/
 
 gulp.task('default', function () {
     runSequence('compile-ts');
